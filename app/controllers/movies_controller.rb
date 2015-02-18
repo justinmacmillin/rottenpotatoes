30cfd71c2f3a1bb4f5ad5fccf0358cc0334b52b8
Justin MacMillin <Justin@Justins-MacBook-Pro.local>
2015-02-17 21:24:57 -0800

    fixed param to params typo

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,10 +29,10 @@ class MoviesController < ApplicationController
         else
           redirect_to movies_path(:sort => params[:sort], :ratings => session[:ratings])
         end
-    elsif session[:sort] or param[:sort]
-      if param[:sort]
+    elsif session[:sort] or params[:sort]
+      if params[:sort]
         @movies = Movie.all
-        @movies.sort_by!{|item|item.send(param[:sort])}
+        @movies.sort_by!{|item|item.send(params[:sort])}
       else
         @movies = Movie.all
         @movies.sort_by!{|item|item.send(session[:sort])}
